8fcea86b506b177cc0729428b09d06ddd795cb30
Tom <totom7700@gmail.com>
2015-10-29 19:07:38 +0100

    kongplayer added

--- app/js/kongregate.js
+++ app/js/kongregate.js
@@ -1,7 +1,16 @@
 var kongregate;
+var kongPlayer;
+
 kongregateAPI.loadAPI(onComplete);
 
 function onComplete() {
     kongregate = kongregateAPI.getAPI();
-    log("Kongregate API loaded");
-}
+    kongPlayer = {
+        name: kongregate.services.getUsername(),
+        id: kongregate.services.getUserID(),
+        token: kongregate.services.getGameAuthToken()
+    }
+    var params = "kongregate_user_id=" + kongPlayer.id + "&kongregate_game_auth_token=" + kongPlayer.token;
+    kongregateAPI.embedFrame("kongregate-game.php?" + params, "kongdiv");
+    log("Kongregate player : " + kongPlayer);
+};
