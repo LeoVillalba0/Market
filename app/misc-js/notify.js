3b6e24953691b0392f06dde56a9d102a08213932
Tom <totom7700@gmail.com>
2015-12-03 19:13:08 +0100

    notify changes (type of notif and autoclose)

--- app/misc-js/notify.js
+++ app/misc-js/notify.js
@@ -1,12 +1,30 @@
 define([], function() {
     var notify = {
+        autoclose: 15000,
+
         close: function() {
             $("#notify-div").fadeOut("slow");
         },
 
-        pop: function(message) {
+        pop: function(type, message) {
+            switch (type) {
+                case "alert":
+                $("#notify-prop").removeClass();
+                $("#notify-prop").addClass("alert alert-warning");
+                    break;
+
+                case "success":
+                $("#notify-prop").removeClass();
+                $("#notify-prop").addClass("alert alert-success");
+                    break;
+            };
+
             $("#notify-message").html(message);
-            $("#notify-div").fadeIn("slow");
+            $("#notify-div").fadeIn("slow", function() {
+                window.setTimeout(function() {
+                    $("#notify-div").fadeOut("slow");
+                }, this.autoclose);
+            });
         },
 
         init: function() {
