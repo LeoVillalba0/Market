3d892d03013d37979d7226a260b7b1300b9bf066
Tom <totom7700@gmail.com>
2015-12-06 21:20:53 +0100

    removed reputation gain on actions

--- app/game/actions.js
+++ app/game/actions.js
@@ -12,9 +12,6 @@ define([], function() {
 		time: [2.5, 7.5, 22.5, 67.5, 202.5, 607.5, 1822.5, 5437.5],
 		timeMultiplier: new Array(),
 		totalTimeMultiplier: 1,
-        reputation: [1, 3, 9, 27, 81, 243, 729, 2187],
-        reputationDivider: 6,
-        currentRep: new Array(),
 
         getTime: function(index) {
             return ((this.time[index] / this.timeMultiplier[index]) / this.totalTimeMultiplier);
@@ -38,10 +35,6 @@ define([], function() {
         	return (reward / time);
         },
 
-        getRep: function(index) {
-            return (this.reputation[index] / this.reputationDivider) * Math.pow(1.01, this.owned[index]);
-        },
-
         upgrade: function(index) {
             var price = this.getPrice(index);
         	if (game.money >= price) {
@@ -60,37 +53,21 @@ define([], function() {
         				var fps = game.options.fps;
         				var time = this.getTime(i);
         				var reward = this.getReward(i);
-                        var rep = this.getRep(i);
 
         				this.progress[i] += times/fps;
         				game.gainMoney(Math.floor(this.progress[i]/time) * reward);
-                        this.currentRep[i] += (Math.floor(this.progress[i]/time) * rep);
-
-                        if (Math.floor(this.progress[i]/time) == 1)
-                            this.display();
 
         				this.progress[i] %= time;
-        				var width = ((this.progress[i]/time) * 100)
-                        var repWidth = ((this.currentRep[i]/this.reputation[i]) * 100);
+
+        				var width = ((this.progress[i]/time) * 100);
 
         				if (time < 0.15)
         					width = 100;
-                        if (this.currentRep[i] >= this.reputation[i]) {
-                            while (this.currentRep[i] >= this.reputation[i]) {
-                                this.currentRep[i] -= this.reputation[i];
-                                game.reputation += this.reputation[i];
-                                game.repLevelUp();
-                            };
-                            this.display();
-                        };
 
         				width = Math.max(width, 1);
-                        repWidth = Math.max(repWidth, 1);
 
         				$("#action-progress-" + (i+1)).css('width', width + '%');
         				$("#action-progress-" + (i+1) + "-info").html(Math.floor(width) + "%");
-                        $("#action-progress-" + (i+1) + "-rep").css('width', repWidth + '%');
-                        $("#action-progress-" + (i+1) + "-rep-info").html(Math.floor(repWidth) + "%");
         			};
         		}
         	};
@@ -102,12 +79,8 @@ define([], function() {
         		var reward = this.getReward(i);
         		var time = this.getTime(i);
         		var perSec = this.getPerSec(i);
-                var rep = this.getRep(i);
-                var currep = this.currentRep[i];
-                var maxrep = this.reputation[i];
         		$("#action-name-" + (i+1)).html(this.list[i] + " (lvl. " + this.owned[i] + ")");
-        		$("#action-info-" + (i+1)).html("+$" + fix(reward) + " <span>($" + fix(perSec, 3) + "/sec)</span><br>+" + fix(rep, 0) +
-                " rep <span>(" + fix(currep, 0) + "/" + fix(maxrep, 0) +")</span><br>" + fix(time) + " sec");
+        		$("#action-info-" + (i+1)).html("+$" + fix(reward) + " <span>($" + fix(perSec, 3) + "/sec)</span><br>" + fix(time) + " sec");
         		$("#action-cost-" + (i+1)).html("Cost $" + fix(price));
         	};
         },
@@ -120,7 +93,6 @@ define([], function() {
         		this.pricePromo.push(0);
         		this.owned.push(0);
         		this.owned[0] = 1;
-                this.currentRep.push(0);
             };
         },
 
