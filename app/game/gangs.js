fd846a296ec0af5d03409ec48b1d2c2a96bde2ac
Tom <totom7700@gmail.com>
2015-12-16 21:03:46 +0100

    small changes

--- app/game/gangs.js
+++ app/game/gangs.js
@@ -1,5 +1,26 @@
 define([], function() {
     var gangs = {
+        stats: {
+            name: undefined
+        },
+
+        members: {
+        },
+
+        display: function() {
+        },
+
+        domInit: function() {
+            this.display();
+        },
+
+        varInit: function() {
+        },
+
+        angularInit: function() {
+            this.domInit();
+        },
+
         init: function() {
             window["game"]["gangs"] = this;
             log("Gangs init.");
