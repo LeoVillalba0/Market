dd1918f51ae2a978e0bb2667557f0240620b6ada
Tom <totom7700@gmail.com>
2016-02-29 14:40:17 +0100

    achievement create function: lvl 200-300-400 are back

--- app/game/achievements.js
+++ app/game/achievements.js
@@ -129,7 +129,7 @@ define(['angular'], function() {
             desc = action + ' at level ' + lvl;
             reqName = 'owned[' + index + ']';
 
-            if (lvl <= 400) {
+            if (lvl === 200 || lvl === 300 || lvl === 400) {
                 desc2 = action + ' speed x2';
                 multiType = 'timeMultiplier[' + index + ']';
                 multi = '*2';
