e0c181ec5ff93779e55a7fc3081e8401465281da
matchello <m.schlesinger84@gmail.com>
2016-03-02 11:59:48 +0100

    Reworked save system

--- app/game/save.js
+++ app/game/save.js
@@ -6,34 +6,52 @@ define(['angular'], function() {
 
         saveInterval: undefined,
 
+        saveVars: [
+            'money',
+            'totalMoney',
+            'allTimeMoney',
+            'level',
+            'reputation',
+            'totalReputation',
+            'allTimeReputation',
+            'reputationNeed',
+            'actions.progress',
+            'actions.owned',
+            'actions.rewardMultiplier',
+            'actions.totalRewardMultiplier',
+            'actions.timeMultiplier',
+            'actions.totalTimeMultiplier',
+            'actions.currentRep',
+            'research.actions.bought',
+            'collections.owned',
+            'options.before',
+            'options.firstTime',
+            'options.version',
+            'options.started'
+        ],
+
+        setObjValByPath: function(obj, val, path) {
+            path = path.split('.');
+            for (i = 0; i < path.length - 1; i++)
+                obj = obj[path[i]];
+
+            obj[path[i]] = val;
+        },
+
+        getObjValByPath: function(obj, path) {
+            var res = path.split('.').reduce(function(val, idx) {
+                return val[idx]
+            }, obj)
+            return res;
+        },
+
         save: function(from) {
-            var toSave = {
-                money: game.money,
-                totalMoney: game.totalMoney,
-                allTimeMoney: game.allTimeMoney,
-                level: game.level,
-                reputation: game.reputation,
-                totalReputation: game.totalReputation,
-                allTimeReputation: game.allTimeReputation,
-                reputationNeed: game.reputationNeed,
 
-                actionsProgress: game.actions.progress,
-                actionsOwned: game.actions.owned,
-                actionsRewardMultiplier: game.actions.rewardMultiplier,
-                actionsTotalRewardMultiplier: game.actions.totalRewardMultiplier,
-                actionsTimeMultiplier: game.actions.timeMultiplier,
-                actionsTotalTimeMultiplier: game.actions.totalTimeMultiplier,
-                actionsCurrentRep: game.actions.currentRep,
+            var toSave = new Object();
 
-                researchActionsBought: game.research.actions.bought,
-
-                collectionsOwned: game.collections.owned,
-
-                optionsBefore: game.options.before,
-                optionsFirstTime: game.options.firstTime,
-                optionsVersion: game.options.version,
-                optionsStarted: game.options.started
-            };
+            for (i = 0; i < this.saveVars.length; i++) {
+                toSave[this.saveVars[i]] = this.getObjValByPath(game, this.saveVars[i]);
+            }
 
             if (game.options.init)
                 localStorage.setItem((this.name + this.salt), JSON.stringify(toSave));
@@ -45,38 +63,27 @@ define(['angular'], function() {
                 log("Game saved.");
         },
 
+        checkExists: function(varToCheck) {
+            if (typeof varToCheck === 'undefined' || varToCheck === null)
+                return false;
+            else
+                return true;
+
+        },
+
         load: function(from) {
+
             if (localStorage.getItem((this.name + this.salt)) === null) {
                 notify.pop("alert", "No save found!");
             } else {
                 var savegame = JSON.parse(localStorage.getItem((this.name + this.salt)));
 
-                game.money = savegame.money;
-                game.totalMoney = savegame.totalMoney;
-                game.allTimeMoney = savegame.allTimeMoney;
-                game.level = savegame.level;
-                game.reputation = savegame.reputation;
-                game.totalReputation = savegame.totalReputation;
-                game.allTimeReputation = savegame.allTimeReputation;
-                game.reputationNeed = savegame.reputationNeed;
+                for (x = 0; x < this.saveVars.length; x++) {
+                    saveVar = this.saveVars[x];
+                    val = savegame[saveVar];
+                    if (this.checkExists(val)) this.setObjValByPath(game, val, this.saveVars[x]);
+                }
 
-                game.actions.progress = savegame.actionsProgress;
-                game.actions.owned = savegame.actionsOwned;
-                game.actions.rewardMultiplier = savegame.actionsRewardMultiplier;
-                game.actions.totalRewardMultiplier = savegame.actionsTotalRewardMultiplier;
-                game.actions.timeMultiplier = savegame.actionsTimeMultiplier;
-                game.actions.totalTimeMultiplier = savegame.actionsTotalTimeMultiplier;
-                game.actions.currentRep = savegame.actionsCurrentRep;
-
-                game.research.actions.bought = savegame.researchActionsBought;
-
-                game.options.before = savegame.optionsBefore;
-                game.options.firstTime = savegame.optionsFirstTime;
-                game.options.started = savegame.optionsStarted;
-
-                game.collections.owned = savegame.collectionsOwned;
-
-                this.saveChecker();
                 game.research.display();
 
                 if (from == 'user')
@@ -84,73 +91,6 @@ define(['angular'], function() {
 
                 if (from !== 'silent')
                     log("Savegame loaded.");
-            };
-        },
-
-        saveChecker: function() {
-            // need to do a smarter function
-
-            var savegame = JSON.parse(localStorage.getItem((game.save.name + game.save.salt)));
-            var saveIntervalID = this.save.saveInterval;
-
-            if (savegame.optionsVersion !== game.options.version) {
-                warn('Loading old savegame, the save may be corrupted.\n' +
-                    'Old : v' + savegame.optionsVersion + ' - current : v' + game.options.version);
-
-                for (var key in game) {
-                    if (typeof game[key] !== "object" && typeof game[key] !== "function") {
-                        if (game[key] == null || game[key] == undefined || isNaN(game[key]) == true) {
-                            warn('bug for ' + key);
-                            game[key] = 0;
-                        };
-                    };
-                };
-
-                for (var key in game.actions) {
-                    if (typeof game.actions[key] !== "object" && typeof game.actions[key] !== "function") {
-                        if (game.actions[key] == null || game.actions[key] == undefined || isNaN(game.actions[key]) == true) {
-                            warn('bug for ' + key);
-                            game.actions[key] = 0;
-                        };
-                    };
-                };
-
-                for (var key in game.achievements) {
-                    if (typeof game.achievements[key] !== "object" && typeof game.achievements[key] !== "function") {
-                        if (game.achievements[key] == null || game.achievements[key] == undefined || isNaN(game.achievements[key]) == true) {
-                            warn('bug for ' + key);
-                            game.achievements[key] = 0;
-                        };
-                    };
-                };
-
-                for (var key in game.collections) {
-                    if (typeof game.collections[key] !== "object" && typeof game.collections[key] !== "function") {
-                        if (game.collections[key] == null || game.collections[key] == undefined || isNaN(game.collections[key]) == true) {
-                            warn('bug for ' + key);
-                            game.collections[key] = 0;
-                        };
-                    };
-                };
-
-                for (var key in game.research.actions) {
-                    if (typeof game.research.actions[key] !== "object" && typeof game.research.actions[key] !== "function") {
-                        if (game.research.actions[key] == null || game.research.actions[key] == undefined || isNaN(game.research.actions[key]) == true) {
-                            warn('bug for ' + key);
-                            game.research.actions[key] = 0;
-                        };
-                    };
-                };
-
-                // for (var key in game.research.production) {
-                //     if (typeof game.research.production[key] !== "object" && typeof game.research.production[key] !== "function") {
-                //         if (game.research.production[key] == null || game.research.production[key] == undefined || isNaN(game.research.production[key]) == true) {
-                //             warn('bug for ' + key);
-                //             window.clearInterval(saveIntervalID);
-                //             game.research.production[key] = 0;
-                //         };
-                //     };
-                // };
             };
         },
 
